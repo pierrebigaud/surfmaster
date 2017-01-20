7335fb4a0317eb15fccd99613de851da18f54823
pierre <pierrebigaud44@orange.fr>
2017-01-21 00:29:40 +0100

    playerMoving

--- Assets/script/player.cs
+++ Assets/script/player.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 
 public class player : MonoBehaviour {
 
-    Rigidbody2D SurfPhysicsBody2D;
+    Rigidbody2D surfPhysicsBody2D;
+    public float vitesse;
+    float inputDir;
 
-	// Use this for initialization
-	void Start () {
-        SurfPhysicsBody2D = transform.GetComponent<Rigidbody2D>();
-        SurfPhysicsBody2D.AddForce(new Vector2(0, 10), ForceMode2D.Impulse);
+    // Use this for initialization
+    void Start () {
+        surfPhysicsBody2D = transform.GetComponent<Rigidbody2D>();
+        surfPhysicsBody2D.AddForce(new Vector2(20, 0), ForceMode2D.Impulse);
     }
 	
 	// Update is called once per frame
 	void Update () {
-        
+        float rotation=
+        inputDir = (float)Input.GetAxisRaw("Horizontal");
+        if (inputDir!=0)
+        {
+            surfPhysicsBody2D.AddTorque(-inputDir/2);
+            surfPhysicsBody2D.AddRelativeForce(new Vector2((surfPhysicsBody2D.velocity.x/-inputDir)/2f, (surfPhysicsBody2D.velocity.y / -inputDir) / 2f));
+            //surfPhysicsBody2D.velocity.Set(Mathf.Abs(transform.rotation.z * surfPhysicsBody2D.velocity.x), Mathf.Abs(transform.rotation.z * surfPhysicsBody2D.velocity.y));
+        }
     }
 }
