44cdfbf5dc1c34fbac9fe400ba5db053b6a62da9
pierre <pierrebigaud44@orange.fr>
2017-01-21 02:54:45 +0100

    score+UI+vitesseMax+deplacementcool

--- Assets/script/gameManager.cs
+++ Assets/script/gameManager.cs
@@ -1,16 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class gameManager : MonoBehaviour {
 
+    public player player;
+    public Text scoreText;
+    int score;
+    int displayedScore;
+    float scoreTime;
+    bool Intervallepasse=true;
+
 	// Use this for initialization
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        scoreText = GameObject.Find("score").GetComponent<Text>();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<player>();
+        Debug.Log("lol");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        score += (int)(Mathf.Pow(player.vitesseY, 2) + Mathf.Pow(player.vitesseY, 2)) / 10;
+        scoreText.text = "SCORE: " + score;
+    }
 }
--- Assets/script/player.cs
+++ Assets/script/player.cs
@@ -1,27 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class player : MonoBehaviour {
 
     Rigidbody2D surfPhysicsBody2D;
-    public float vitesse;
+    public float vitesseY;
+    public float vitesseX;
+    public static float vitesseMax=200;
+    public Text speedText;
     float inputDir;
 
     // Use this for initialization
     void Start () {
         surfPhysicsBody2D = transform.GetComponent<Rigidbody2D>();
         surfPhysicsBody2D.AddForce(new Vector2(20, 0), ForceMode2D.Impulse);
+        speedText = GameObject.Find("speed").GetComponent<Text>();
     }
 	
 	// Update is called once per frame
 	void Update () {
-        float rotation=
         inputDir = (float)Input.GetAxisRaw("Horizontal");
         if (inputDir!=0)
         {
-            surfPhysicsBody2D.AddTorque(-inputDir/2);
-            surfPhysicsBody2D.AddRelativeForce(new Vector2((surfPhysicsBody2D.velocity.x/-inputDir)/2f, (surfPhysicsBody2D.velocity.y / -inputDir) / 2f));
+            if (surfPhysicsBody2D.velocity.x < -vitesseMax)
+            {
+                vitesseX = -vitesseMax;
+            }
+            else if (surfPhysicsBody2D.velocity.x > vitesseMax)
+            {
+                vitesseX = vitesseMax;
+            }
+            else
+            {
+                vitesseX = surfPhysicsBody2D.velocity.x;
+            }
+            if (surfPhysicsBody2D.velocity.x < -vitesseMax)
+            {
+                vitesseY = -vitesseMax;
+            }
+            else if (surfPhysicsBody2D.velocity.y > vitesseMax)
+            {
+                vitesseY = vitesseMax;
+            }
+            else
+            {
+                vitesseY = surfPhysicsBody2D.velocity.y;
+            }
+
+            speedText.text = "SPEED: "+(int)(vitesseY+vitesseX);
+            surfPhysicsBody2D.AddTorque(-inputDir / 2);
+            surfPhysicsBody2D.AddRelativeForce(new Vector2((vitesseX / -inputDir)/2f, (vitesseY / -inputDir) / 2f));
             //surfPhysicsBody2D.velocity.Set(Mathf.Abs(transform.rotation.z * surfPhysicsBody2D.velocity.x), Mathf.Abs(transform.rotation.z * surfPhysicsBody2D.velocity.y));
         }
     }
