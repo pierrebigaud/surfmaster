146daf86811f8465b71b2621441ffb4d4b1cc233
ayoussef <anthony.youssef@telecomnancy.eu>
2017-01-21 01:00:54 +0100

    Ajout de la fonction de generation des vagues

---
+++ Assets/Scripts/OnContactWithPlayer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnContactWithPlayer : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other){ //It means that the player touches the wave
+		if (other.tag == "Player") {
+	}
+	
+	}
+}
---
+++ Assets/Scripts/WaveSpawnScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawnScript : MonoBehaviour {
+
+    public GameObject Wave;
+	public Quaternion SpawnQuat = new Quaternion(); 
+
+	void Start() {
+		SpawnWaves();
+	}
+
+	Vector3 RandomPost() {
+		Vector3 toReturn = new Vector3 ();
+		toReturn = Random.insideUnitCircle * 5;
+		return toReturn;
+	}
+
+	void SpawnWaves(){
+		Instantiate(Wave, RandomPost(),SpawnQuat);
+		Instantiate(Wave, RandomPost(),SpawnQuat);
+		Instantiate(Wave, RandomPost(),SpawnQuat);
+		Instantiate(Wave, RandomPost(),SpawnQuat);
+		Instantiate(Wave, RandomPost(),SpawnQuat);
+	}
+}
