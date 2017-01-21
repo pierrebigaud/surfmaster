7e0a7c05897502ab8149e1216daa058d73283f8d
Niirok <anthony_youssef@hotmail.fr>
2017-01-21 02:38:32 +0100

    minor enhancement to waves behaviour

--- Assets/Scripts/WaveSpawnScript.cs
+++ Assets/Scripts/WaveSpawnScript.cs
@@ -3,25 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class WaveSpawnScript : MonoBehaviour {
+	static int MAXBORDERLIMIT = 50;
+	static int MINBORDERLIMIT = -MAXBORDERLIMIT;
+	static int MAXWAVES = 100;
+
+	public float spawnWait = 1;
 
     public GameObject Wave;
 	public Quaternion SpawnQuat = new Quaternion(); 
 
 	void Start() {
-		SpawnWaves();
+		QuickStart();
+		StartCoroutine (SpawnWaves());
 	}
 
 	Vector3 RandomPost() {
-		Vector3 toReturn = new Vector3 ();
-		toReturn = Random.insideUnitCircle * 5;
+		Vector3 toReturn = new Vector3 (Random.Range(MINBORDERLIMIT,MAXBORDERLIMIT),Random.Range(MINBORDERLIMIT,MAXBORDERLIMIT),0);
+		//toReturn = Random.insideUnitCircle * 5;
 		return toReturn;
 	}
 
-	void SpawnWaves(){
-		Instantiate(Wave, RandomPost(),SpawnQuat);
-		Instantiate(Wave, RandomPost(),SpawnQuat);
-		Instantiate(Wave, RandomPost(),SpawnQuat);
-		Instantiate(Wave, RandomPost(),SpawnQuat);
-		Instantiate(Wave, RandomPost(),SpawnQuat);
+	IEnumerator SpawnWaves(){
+		for (int i = 0; i < MAXWAVES; i++) {
+			Instantiate (Wave, RandomPost (), SpawnQuat);
+			yield return new WaitForSeconds(spawnWait/5);
+		}
+	}
+
+	void QuickStart(){
+		for (int i = 0; i < MAXWAVES; i++) {
+			Instantiate (Wave, RandomPost (), SpawnQuat);
+
+		}
+	
+	
 	}
 }
